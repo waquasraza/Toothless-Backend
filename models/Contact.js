29ce4b75e65073045a0d6259eaa0533efb31ef93
Waquas Raza <thewaquas@gmail.com>
2024-02-29 18:49:28 +0530

    Feature Addition Plan

--- models/Contact.js
+++ models/Contact.js
@@ -1,29 +1,67 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
+
 	first_name: {
 		type: String,
 		required: true,
 	},
+	
 	last_name: {
 		type: String,
 	},
+
 	email: {
 		type: String,
 		required: true,
 		unique: true,
 	},
+
 	phone: {
 		type: String,
 		required: true,
 		unique: true,
 	},
+
 	gender: {
 		type: String,
 	},
+
 	nationality: {
 		type: String,
 	},
+	
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
+
+
+/*
+contact type
+timezone
+tags
+lists
+company
+industry
+date of birth
+custom fields
+contact source
+contact type - lead/customer
+
+custom fields: 
+address - 
+map url
+
+
+feature: 
+smart list 
+ - tag
+ - list
+
+
+Taxanomy - Category and tags
+
+list - category
+
+
+*/
